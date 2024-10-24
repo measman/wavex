04af81b8bf72845336fcf4026095cdb4f4094e75
Asman123 <tdkasman2@gmail.com>
2024-10-24 16:53:40 +0545

    init prj with flowbite

---
+++ app/Http/Middleware/redirectAdmin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class redirectAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, $gaurd = null): Response
+    {
+        if(Auth::gaurd($gaurd)->check() && Auth::user()->isAdmin = 1){
+            return redirect()->route('admin.dashboard');
+        }
+        return $next($request);
+    }
+}
