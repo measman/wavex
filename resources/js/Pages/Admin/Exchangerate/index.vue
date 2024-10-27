6416bc740ab636728e9888e28cd385901bc9c155
sushan shrestha <sushan015528288@gmail.com>
2024-10-27 16:44:21 +0545

    exchange rate in process

---
+++ resources/js/Pages/Admin/Exchangerate/index.vue
@@ -0,0 +1,9 @@
+<script setup>
+import AdminLayout from '../Components/AdminLayout.vue';
+import ExchangeRateList from './Exchangerlist.vue';
+</script>
+<template>
+    <AdminLayout>
+        <ExchangeRateList />    
+    </AdminLayout>
+</template>
