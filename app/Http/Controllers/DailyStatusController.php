1627a79ba96979ec51b01f90099bd5d4175d83d9
sushan shrestha <sushan015528288@gmail.com>
2024-11-11 16:56:04 +0545

    after day close wallet data insertion

--- app/Http/Controllers/DailyStatusController.php
+++ app/Http/Controllers/DailyStatusController.php
@@ -4,25 +4,56 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\DailyStatusResource;
 use App\Models\DailyStatus;
+use App\Models\Transaction;
+use App\Models\Wallet;
 use Illuminate\Http\Client\Request as ClientRequest;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class DailyStatusController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $daily_status = DailyStatus::orderBy('date', 'desc')->first();
-        return Inertia::render('Admin/Dailystatus/index', ['dailystatus'=>$daily_status]);
-
+        return Inertia::render('Admin/Dailystatus/index', ['dailystatus' => $daily_status]);
     }
 
     public function insertstatus($id)
     {
         $dailystatus = DailyStatus::findOrFail($id);
-        $dailystatus->update([
-            'status' => 'day_end',
-        ]);
+        $log_date = $dailystatus->date;
+        // $dailystatus->update([
+        //     'status' => 'day_end',
+        // ]);
+        $userWalletBalances = [];
+        $transactionlogs = Transaction::whereDate('created_at', $log_date)
+            ->where('status', 'Completed')
+            ->get();
+        foreach ($transactionlogs as $transaction) {
+            if (!isset($userWalletBalances[$transaction->user_id])) {
+                $userWalletBalances[$transaction->user_id] = [];
+            }
+            if (!isset($userWalletBalances[$transaction->user_id][$transaction->from_wallet_id])) {
+                $userWalletBalances[$transaction->user_id][$transaction->from_wallet_id] = 0;
+            }
+            $userWalletBalances[$transaction->user_id][$transaction->from_wallet_id] += $transaction->from_amount;
+            if (!isset($userWalletBalances[$transaction->user_id][$transaction->to_wallet_id])) {
+                $userWalletBalances[$transaction->user_id][$transaction->to_wallet_id] = 0;
+            }
+            $userWalletBalances[$transaction->user_id][$transaction->to_wallet_id] -= $transaction->to_amount;
+        }
+        dd($userWalletBalances);
+        foreach ($userWalletBalances as $userId => $wallets) {
+            $id = $userId;
+            foreach ($wallets as $walletId => $balance) {
+                dd($balance,$walletId);
+                Wallet::create([
+                    'user_id' => $id,
+                    'currency_id' => $walletId,
+                    'balance' => $balance,
+                ]);
+            }
+        }
         return redirect()->route('dailystatus.index');
     }
-    
 }
