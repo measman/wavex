3cd48e803f378be0e0ca27d4326b079c74135b5e
sushan shrestha <sushan015528288@gmail.com>
2024-11-18 12:04:18 +0545

    dashboard todays exchange rate added

--- resources/js/Pages/Admin/Dashboard.vue
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -9,13 +9,80 @@ onMounted(() => {
     initFlowbite();
 });
 defineProps({
-    daily_status:Object,
-    todayexchangerate:Object,
+    daily_status: Object,
+    todayexchangerate: Object,
 })
 //let daily_status=usePage().props.daily_status.data;
 </script>
 <template>
-<AdminLayout>
-    <h1 class="text-xl font-bold mb-4">Daily Status : {{ daily_status.data.status }}</h1> 
-</AdminLayout>
+    <AdminLayout>
+        <h1 class="text-xl font-bold mb-4">Daily Status : {{ daily_status.data.status }}</h1>
+        <div class="bg-gray-100 py-10">
+            <div class="mx-auto max-w-7xl">
+                <div class="px-4 sm:px-6 lg:px-8">
+                    <div class="sm:flex sm:items-center">
+                        <div class="sm:flex-auto">
+                            <h1 class="text-xl font-semibold text-gray-900">
+                                Today's ExchangeRate
+                            </h1>
+
+                        </div>
+                    </div>
+
+                    <div class="mt-8 flex flex-col">
+                        <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                            <div class="relative text-sm text-gray-800 col-span-3">
+                                <div
+                                    class="absolute pl-2 left-0 top-0 bottom-0 flex items-center pointer-events-none text-gray-500">
+                                </div>
+
+                            </div>
+
+                            <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+                                <div
+                                    class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg relative">
+                                    <table class="min-w-full divide-y divide-gray-300">
+                                        <thead class="bg-gray-50">
+                                            <tr>
+                                                <th scope="col"
+                                                    class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
+                                                    Currency</th>
+                                                <th scope="col"
+                                                    class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
+                                                    Unit</th>
+                                                <th scope="col"
+                                                    class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
+                                                    Buy</th>
+                                                <th scope="col"
+                                                    class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                                    Sell
+                                                </th>
+                                            </tr>
+                                        </thead>
+                                        <tbody class="divide-y divide-gray-200 bg-white">
+                                            <tr v-for="ter in todayexchangerate">
+                                                <td
+                                                    class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
+                                                    {{ ter.currency.iso3 }}</td>
+                                                <td
+                                                    class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
+                                                    {{ ter.currency.unit }}</td>
+                                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{
+                                                    ter.buy }}</td>
+                                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{
+                                                    ter.sell
+                                                    }}</td>
+
+                                            </tr>
+
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AdminLayout>
 </template>
