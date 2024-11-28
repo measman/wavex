6090b241f0003318f4b3ca99c0574790b302df6b
sushan shrestha <sushan015528288@gmail.com>
2024-11-28 14:48:35 +0545

    Filter in Transaction

--- app/Http/Controllers/Api/TransactionApiController.php
+++ app/Http/Controllers/Api/TransactionApiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Transaction;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class TransactionApiController extends Controller
 {
@@ -36,6 +37,14 @@ class TransactionApiController extends Controller
         $del = Transaction::find($id);
         $del->delete();
         return response()->json(['message' => 'Transaction deleted successfully.']);
+    }
+
+    public function transactionsearch(Request $request){
+        $id = $request->input('id');
+        $status = $request->input('status');
+        $type = $request->input('type');
+        $data = $this->transaction->fetchallforsearch($id,$status,$type);
+        return response()->json(['data' => $data]);
     }
     
 }
